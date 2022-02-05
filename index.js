5330f0dbfed1e0c175e09814b5fbf1513069eda8
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2022-02-05 08:07:57 +0100

    push

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://ftx.com');
+  await page.goto('http://paperdaz-dev.herokuapp.com');
   await page.screenshot({ path: 'example.png' });
+  await page.pdf({ path: 'hn.pdf', format: 'a4' });
 
   await browser.close();
 })();
