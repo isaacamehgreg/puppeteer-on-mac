1121300c989c687c99b982e6eba9fd5d6b2d27b9
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2022-02-12 07:01:40 +0100

    mac

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto('http://paperdaz-dev.herokuapp.com');
-  await page.screenshot({ path: 'example.png' });
-  await page.pdf({ path: 'test.pdf', format: 'a4' });
+  await page.goto('https://accounts.binance.com/en/login');
 
-  await browser.close();
+  await page.screenshot({ path: 'example.png' });
+
+  await page.type('input[name=email]', 'tst@g.com')
+
+  await page.type('input[name=password]', '12345678')
+
+  await page.click('#click_login_submit')
+
+  console.log('done')
+
+  //await page.click('[data-test-foo4="true"]');
+
+  //await browser.close();
 })();
